6f14f2101f510eaca6f9138d53f3585fc0ea6c83
gradooid <graduses9@gmail.com>
2019-03-25 23:32:29 +0200

    Updated index.php.

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
-$token = '841678027:AAFlwWoWNL7kX0JNZ-j1Wa0SMAZtqSTg_Y8';
-$chat_id = '-280089425';
+$token = "841678027:AAFlwWoWNL7kX0JNZ-j1Wa0SMAZtqSTg_Y8";
+$chat_id = "-280089425";
 $arr = array(
   'username: ' => $name,
   'phone: ' => $phone
 );
 
 foreach($arr as $key => $value) {
-  $txt .= '<b>'.$key.'</b>'.$value.'%0A';
+  $txt .= "<b>".$key."</b>".$value."%0A";
 };
 
-$sendToTelegram = fopen('https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
-  &parse_mode=html&text={$txt}', 'r');
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}
+  &parse_mode=html&text={$txt}","r");
 
   if ($sendToTelegram) {
     echo '<p>Success</p>';
